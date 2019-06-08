74bcc62b0e6cb4b679d7328b074ed8ccb850c65a
Łukasz Kwiecień <lukaskwkw@gmail.com>
2019-06-08 16:46:57 +0200

    Remove obsolete math tab

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,14 @@
 import Home from "./pages/Home";
 import FourOhFour from "./pages/FourOhFour";
 import Portfolio from "./pages/Portfolio";
-import MathTab from "./pages/MathPage/";
+// import MathTab from "./pages/MathPage/";
 import VectorTab from "./pages/VectorPage";
 
 const PAGES = {
   "/": Home,
   "/404": FourOhFour,
   "/portfolio": Portfolio,
-  "/math": MathTab,
+  // "/math": MathTab,
   "/vector": VectorTab
 };
 
